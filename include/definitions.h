5960bfc04426cd46abcfcb0a150ec58613def3cd
AngeloDamante <angelo.damante16@gmail.com>
2023-08-05 16:51:03 +0200

    feat: add pin declarations

--- include/definitions.h
+++ include/definitions.h
@@ -5,4 +5,18 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
+// Servo
+const int PIN_SERVO = 10;
+
+// US
+const int PIN_ECHO = 12;
+const int PIN_TRIG = 13;
+
+// Motors
+const int PIN_PWM_RIGHT = 5;
+const int PIN_PWM_LEFT = 6;
+const int PIN_IN_RIGHT = 7;
+const int PIN_IN_LEFT = 8;
+const int PIN_STBY = 3;
+
 #endif //DEFINITIONS_H
