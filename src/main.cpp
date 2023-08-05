35fa1a57e964fa6620c7355518c7a6da49244aa1
AngeloDamante <angelo.damante16@gmail.com>
2023-08-05 16:51:31 +0200

    refactor: clean code

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,32 @@
 #include <Servo.h>
 #include "UltrasonicSensor.h"
 #include "MotorDriver.h"
-#include "demo_motors.h"
+#include "definitions.h"
+#include "demo_libraries.h"
 
 // Servo
 Servo myServo;
-const int PIN_SERVO = 9;
 
-// US
-const int PIN_ECHO = 12;
-const int PIN_TRIG = 13;
-
+// US;
 int distanceCm;
 UltrasonicSensor us(PIN_TRIG, PIN_ECHO);
 
 // Motors
-const int PIN_PWM_RIGHT = 5;
-const int PIN_PWM_LEFT = 6;
-const int PIN_IN_RIGHT = 7;
-const int PIN_IN_LEFT = 8;
-const int PIN_STBY = 3;
 MotorDriver md(PIN_IN_RIGHT, PIN_IN_LEFT, PIN_PWM_RIGHT, PIN_PWM_LEFT, PIN_STBY);
 
 void setup()
 {
+    Serial.begin(9600);
+    Serial.println("Hello");
+
+    // init servo
     myServo.attach(PIN_SERVO);
     myServo.write(0);
 
-    Serial.begin(9600);
-    Serial.println("Hello");
-//    us.init();
-
-    demo_motors();
+//    demo_motors();
 }
 
 __attribute__((unused)) void loop()
 {
-    // US
-    distanceCm = us.computeDistance();
-    Serial.println(distanceCm);
-    delay(100);
+    // code in forever loop
 }
