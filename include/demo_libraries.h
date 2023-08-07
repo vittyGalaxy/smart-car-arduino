39d7f1b157220d2ab54f6fb6218d256896492721
AngeloDamante <angelo.damante16@gmail.com>
2023-08-07 12:24:01 +0200

    fix: add tutorial

--- include/demo_libraries.h
+++ include/demo_libraries.h
@@ -2,6 +2,9 @@
 /**
 * @file: demo_motors.cpp
 * @description: Demo for us and motors
+*                   1. create an object
+*                   2. init this object
+*                   3. use object
 * @author: Vittorio
 * @mail:
 */
@@ -10,37 +13,47 @@
 #ifndef DEMO_MOTORS_H
 #define DEMO_MOTORS_H
 
+
 /*****************************************************************************/
-extern MotorDriver md;
+//extern MotorDriver md;
+
+// 1. create an object
+MotorDriver oMd(PIN_IN_RIGHT, PIN_IN_LEFT, PIN_PWM_RIGHT, PIN_PWM_LEFT, PIN_STBY);
 
 void demo_motors() {
-    md.init();
-    md.on();
 
+    // 2. initialize
+    oMd.init();
+    oMd.on();
+
+    // 3. use object
     // right
-    md.go(Motor::RIGHT, 150);
+    oMd.go(Motor::RIGHT, 150);
     delay(10000);
-    md.stop(Motor::RIGHT);
+    oMd.stop(Motor::RIGHT);
 
     // left
-    md.go(Motor::LEFT, 150);
+    oMd.go(Motor::LEFT, 150);
     delay(10000);
-    md.stop(Motor::LEFT);
+    oMd.stop(Motor::LEFT);
 
     // right and left
-    md.go(Motor::RIGHT, 150);
-    md.go(Motor::LEFT, 150);
+    oMd.go(Motor::RIGHT, 150);
+    oMd.go(Motor::LEFT, 150);
     delay(10000);
-    md.stop(Motor::LEFT);
-    md.stop(Motor::RIGHT);
+    oMd.stop(Motor::LEFT);
+    oMd.stop(Motor::RIGHT);
 }
 
 /*****************************************************************************/
-extern UltrasonicSensor us;
+//extern UltrasonicSensor us;
+
+// 1. create an object
+UltrasonicSensor oUs(PIN_TRIG, PIN_ECHO);
 
 void demo_ultrasonicSensor() {
-    us.init();
-    int distanceCm = us.computeDistance();
+    oUs.init(); // 2.init
+    int distanceCm = oUs.computeDistance(); // 3. use
     Serial.println(distanceCm);
 }
 
