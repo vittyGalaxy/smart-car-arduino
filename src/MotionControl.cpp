a17046f1528045c9951c1224292616ed40dd4868
vittyGalaxy <tiozzovittorio25@gmail.com>
2023-08-10 16:47:33 +0200

    TODO removed

--- src/MotionControl.cpp
+++ src/MotionControl.cpp
@@ -10,55 +10,46 @@
 #include "MotionControl.h"
 
 void MotionControl::forward(int speed) {
-    //TODO
     md.go(Motor::RIGHT, speed);
     md.go(Motor::LEFT, speed);
 }
 
 void MotionControl::backward(int speed) {
-    //TODO
     md.back(Motor::RIGHT, speed);
     md.back(Motor::LEFT, speed);
 }
 
 void MotionControl::left(int speed) {
-    //TODO
     md.go(Motor::RIGHT, speed);
     md.back(Motor::LEFT, speed);
 }
 
 void MotionControl::right(int speed) {
-    //TODO
     md.go(Motor::LEFT, speed);
     md.back(Motor::RIGHT, speed);
 }
 
 void MotionControl::leftForward(int speed) {
-    //TODO
     md.go(Motor::RIGHT, speed);
     md.stop(Motor::LEFT);
 }
 
 void MotionControl::leftBackward(int speed) {
-    //TODO
     md.back(Motor::RIGHT, speed);
     md.stop(Motor::LEFT);
 }
 
 void MotionControl::rightForward(int speed) {
-    //TODO
     md.go(Motor::LEFT, speed);
     md.stop(Motor::RIGHT);
 }
 
 void MotionControl::rightBackward(int speed) {
-    //TODO
     md.back(Motor::LEFT, speed);
     md.stop(Motor::RIGHT);
 }
 
 void MotionControl::stop() {
-    //TODO
     md.stop(Motor::RIGHT);
     md.stop(Motor::LEFT);
 }
