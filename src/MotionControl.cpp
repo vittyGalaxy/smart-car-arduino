380ff72136163f51b934c6dfc071b39e4ca3d2de
vittyGalaxy <tiozzovittorio25@gmail.com>
2023-08-11 12:38:31 +0200

    add led for each function

--- src/MotionControl.cpp
+++ src/MotionControl.cpp
@@ -8,48 +8,59 @@
 /********************************************************************************/
 
 #include "MotionControl.h"
+#include "RGBLed.h"
+RGBLed led;
 
 void MotionControl::forward(int speed) {
     md.go(Motor::RIGHT, speed);
     md.go(Motor::LEFT, speed);
+    led.show(0, 255, 0);
 }
 
 void MotionControl::backward(int speed) {
     md.back(Motor::RIGHT, speed);
     md.back(Motor::LEFT, speed);
+    led.show(255, 0, 0);
 }
 
 void MotionControl::left(int speed) {
     md.go(Motor::RIGHT, speed);
     md.back(Motor::LEFT, speed);
+    led.show(247, 152, 27);
 }
 
 void MotionControl::right(int speed) {
     md.go(Motor::LEFT, speed);
     md.back(Motor::RIGHT, speed);
+    led.show(11, 252, 252);
 }
 
 void MotionControl::leftForward(int speed) {
     md.go(Motor::RIGHT, speed);
     md.stop(Motor::LEFT);
+    led.show(247, 233, 27);
 }
 
 void MotionControl::leftBackward(int speed) {
     md.back(Motor::RIGHT, speed);
     md.stop(Motor::LEFT);
+    led.show(0, 0, 255);
 }
 
 void MotionControl::rightForward(int speed) {
     md.go(Motor::LEFT, speed);
     md.stop(Motor::RIGHT);
+    led.show(131, 11, 252);
 }
 
 void MotionControl::rightBackward(int speed) {
     md.back(Motor::LEFT, speed);
     md.stop(Motor::RIGHT);
+    led.show(252, 11, 156);
 }
 
 void MotionControl::stop() {
     md.stop(Motor::RIGHT);
     md.stop(Motor::LEFT);
+    led.show(152, 27, 247);
 }
