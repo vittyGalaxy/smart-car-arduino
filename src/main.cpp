c75c7157ae94d46e292980a2a7e9aebef49e4e8d
AngeloDamante <angelo.damante16@gmail.com>
2023-08-14 11:53:50 +0200

    wip: working for remote control

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,17 @@
 #include <Arduino.h>
-#include <Servo.h>
 #include <IRremote.h>
-
-//#include "UltrasonicSensor.h"
 #include "definitions.h"
 //#include "demo_libraries.h"
-#include "MotionControl.h"
-#include "RGBLed.h"
-//#include "mission.h"
-//MotionControl aserial;
 
-//RGBLed led;
-// Servo
-Servo myServo;
-
-int receiver = PIN_IR; // pin 1 of IR receiver to Arduino digital pin 11
-IRrecv irrecv(receiver); // create instance of 'irrecv'
-decode_results results;
-
-void setup()
-{
+void setup() {
     Serial.begin(9600);
     Serial.println("Hello");
-    irrecv.enableIRIn(); // Start the receiver
+    IrReceiver.begin(PIN_IR); // Start the receiver
 }
 
-__attribute__((unused)) void loop()
-{
-    if (irrecv.decode(&results)) // have we received an IR signal?
-    {
-        Serial.println(results.value, HEX); // display it on serial monitor in hexadecimal
-        irrecv.resume();// receive the next value
+__attribute__((unused)) void loop() {
+    if (IrReceiver.decode()) {
+        Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+        IrReceiver.resume();
     }
 }
