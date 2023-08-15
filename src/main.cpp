a1704c5f54bdc0a6a99bd73f8c78149a71eca5cd
vittyGalaxy <tiozzovittorio25@gmail.com>
2023-08-15 02:41:46 +0200

    Working for remote control

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <Arduino.h>
 #include <IRremote.h>
 #include "definitions.h"
+#include "Mission.h"
 //#include "demo_libraries.h"
+
+const uint32_t buttonOK = 0xBF40FF00;
+const uint32_t buttonUp = 0xB946FF00;
+const uint32_t buttonDown = 0xEA15FF00;
+const uint32_t buttonRight = 0xBC43FF00;
+const uint32_t buttonLeft = 0xBB44FF00;
 
 void setup() {
     Serial.begin(9600);
@@ -10,8 +17,10 @@ void setup() {
 }
 
 __attribute__((unused)) void loop() {
-    if (IrReceiver.decode()) {
-        Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
-        IrReceiver.resume();
+    switch (buttonOK){
+        case buttonUp:
+            md.forward(150);
+
+
     }
 }
