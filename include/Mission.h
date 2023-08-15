705be84894956c03fde7ce74bf30536f90c20811
vittyGalaxy <tiozzovittorio25@gmail.com>
2023-08-15 02:57:06 +0200

    added 2 new files

---
+++ include/Mission.h
@@ -0,0 +1,22 @@
+/********************************************************************************/
+/**
+ * @file: Mission.h
+ * @description: Mission.h declaration
+ * @authors: AngeloDamante
+ * @authors: Vittorio Tiozzo
+ */
+/*******************************************************************************/
+
+#ifndef SMART_CAR_ARDUINO_MISSION_H
+#define SMART_CAR_ARDUINO_MISSION_H
+/********************************************************************************/
+
+#include "UltrasonicSensor.h"
+#include "MotionControl.h"
+
+MotionControl md(PIN_IN_RIGHT, PIN_IN_LEFT, PIN_PWM_RIGHT, PIN_PWM_LEFT, PIN_STBY);
+UltrasonicSensor us(PIN_TRIG, PIN_ECHO);
+
+/*****************************************************************************/
+
+#endif //SMART_CAR_ARDUINO_MISSION_H
---
+++ src/Mission.cpp
@@ -0,0 +1,10 @@
+/********************************************************************************/
+/**
+ * @file: Mission.cpp
+ * @description: Mission.cpp initialization
+ * @authors: AngeloDamante
+ * @authors: Vittorio Tiozzo
+ */
+ /*******************************************************************************/
+
+#include "mission.h"
