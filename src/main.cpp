f78821e90cf3cc8d89e0eee106d17688bcecb127
vittyGalaxy <tiozzovittorio25@gmail.com>
2023-08-15 03:38:02 +0200

    main implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <Arduino.h>
 #include <IRremote.h>
 #include "definitions.h"
+#include "MotionControl.h"
 //#include "demo_libraries.h"
+
+MotionControl aserial;
+unsigned long preMillis;
 
 const uint32_t buttonOK = 0xBF40FF00;
 const uint32_t buttonUp = 0xB946FF00;
@@ -17,31 +21,47 @@ void setup() {
 
 __attribute__((unused)) void loop() {
     if (IrReceiver.decode()) {
+        preMillis = millis();
+        IrReceiver.resume(); // Receive the next value
         switch (IrReceiver.decodedIRData.decodedRawData) {
             case buttonOK:
                 /* TODO */
+                aserial.stop();
+//                delay(100);
                 break;
 
             case buttonUp:
                 /* TODO */
+                aserial.forward(150);
+//                delay(100);
                 break;
 
             case buttonDown:
                 /* TODO */
+                aserial.backward(150);
+//                delay(100);
                 break;
 
             case buttonRight:
                 /* TODO */
+                aserial.right(65);
+//                delay(100);
                 break;
 
             case buttonLeft:
                 /* TODO */
+                aserial.left(65);
+//                delay(100);
                 break;
 
             default:
                 break;
         }
-        IrReceiver.resume(); // Receive the next value
+//        delay(100);
+    }else{
+        if(millis() - preMillis > 300){
+            aserial.stop();
+            preMillis = millis();
+        }
     }
-
 }
