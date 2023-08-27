00320cc25c3e0a0165bc75057782093d7928ca06
vittyGalaxy <tiozzovittorio25@gmail.com>
2023-08-27 19:07:18 +0200

    main implementing

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,16 @@ int distanceCm;
 
 MotionControl aserial;
 unsigned long preMillis;
+uint32_t commandReceived = 0;
 
 const uint32_t buttonOK = 0xBF40FF00;
 const uint32_t buttonUp = 0xB946FF00;
 const uint32_t buttonDown = 0xEA15FF00;
 const uint32_t buttonRight = 0xBC43FF00;
 const uint32_t buttonLeft = 0xBB44FF00;
+
+uint32_t Commands[] = {buttonOK, buttonUp, buttonDown, buttonRight, buttonLeft};
+bool isCommand(uint32_t command);
 
 void setup() {
     Serial.begin(9600);
@@ -36,49 +40,79 @@ void setup() {
  */
 __attribute__((unused)) void loop() {
     distanceCm = us.computeDistance();
-    Serial.println(distanceCm);
+//    Serial.println(distanceCm);
+
     if (IrReceiver.decode()) {
-        preMillis = millis();
         IrReceiver.resume(); // Receive the next value
-        switch (IrReceiver.decodedIRData.decodedRawData) {
+
+        uint32_t buttonReceived = IrReceiver.decodedIRData.decodedRawData;
+
+        if (!isCommand(buttonReceived) && commandReceived != 0) {
+            buttonReceived = commandReceived;
+        }
+
+        preMillis = millis();
+
+        switch (buttonReceived) {
             case buttonOK:
                 aserial.stop();
+                Serial.println("OK");
+
+                commandReceived = buttonOK;
                 Voltage_Measure(PIN_BT);
                 break;
 
             case buttonUp:
-                // FIXME
+                distanceCm = us.computeDistance();
+                Serial.println("UP");
+
                 if(distanceCm < 30){
-                    Serial.println("ostacolo trovato");
-                    aserial.forward(25);
-                    delay(100);
+                    aserial.forward(50);
                 }else{
-                    Serial.println("ostacolo non trovato");
                     aserial.forward(150);
-                    delay(100);
                 }
+                commandReceived = buttonUp;
+//                    IrReceiver.resume(); // Receive the next value
                 break;
 
             case buttonDown:
+                Serial.println("DOWN");
+
                 aserial.backward(150);
+                commandReceived = buttonDown;
                 break;
 
             case buttonRight:
+                Serial.println("RIGHT");
+
                 aserial.right(65);
+                commandReceived = buttonRight;
                 break;
 
             case buttonLeft:
+                Serial.println("LEFT");
+
                 aserial.left(65);
+                commandReceived = buttonLeft;
                 break;
 
             default:
+                Serial.println(buttonReceived, HEX);
                 break;
         }
     }else{
         if(millis() - preMillis > 200){
-            // FIXME
             aserial.stop();
             preMillis = millis();
         }
     }
+}
+
+bool isCommand(uint32_t command) {
+    for (uint32_t cmd : Commands) {
+        if (cmd == command) {
+            return true;
+        }
+    }
+    return false;
 }
